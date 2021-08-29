788154ff0b20ffde245157a7e32cc3f6c86a51df
Ryuminah <hellorma12@gmail.com>
2021-08-29 23:34:39 +0900

    [DFS] 음료수 얼려먹기3
    다시 구현

--- Algorithm/Project/DFS.cpp
+++ Algorithm/Project/DFS.cpp
@@ -128,145 +128,58 @@ void DFS()
 	Graph.clear();
 }
 
+
+
+bool CheckIceBox(int _x, int _y, int _n, int _m, int _Graph[][1000])
+{
+	if (_x <= -1 || _x >= _n || _y <= -1 || _y >= _m)
+	{
+		return false;
+	}
+	// 현재 노드를 아직 방문하지 않았다면
+	if (_Graph[_x][_y] == 0)
+	{
+		// 해당 노드 방문 처리
+		_Graph[_x][_y] = 1;
+
+		// 상, 하, 좌, 우의 위치들도 모두 재귀적으로 호출
+		CheckIceBox(_x - 1, _y,_n,_m, _Graph);
+		CheckIceBox(_x, _y - 1,_n,_m,_Graph);
+		CheckIceBox(_x + 1, _y, _n, _m, _Graph);
+		CheckIceBox(_x, _y + 1, _n, _m, _Graph);
+		return true;
+	}
+	return false;
+}
+
 void FreezingDrinks()
 {
-	enum Position {X,Y};
-	enum Direction { R, L, U, D};
-
-	struct Ice
-	{
-		bool m_Check = false;
-		int m_Ice;
-		int pos[2];
-	};
-
 	int N, M;
-	int result = 0;
+	int Graph[1000][1000];
 	std::cin >> N >> M;
-	std::cin.ignore();
 
-	std::vector<Ice*> IceBox;
-
-	// 맵 채우기
-	for (int y = 0; y < M; ++y)
+	// 2차원 리스트의 맵 정보 입력 받기
+	for (int i = 0; i < N; i++)
 	{
-		for (int x = 0; x  < N; ++x)
+		for (int j = 0; j < M; j++)
 		{
-			Ice* pIce = new Ice;
-			pIce->pos[X] = x;
-			pIce->pos[Y] = y;
-			pIce->m_Ice = (int)std::cin.get() - '0';
-
-			IceBox.push_back(pIce);
-			
+			scanf("%1d", &Graph[i][j]);
 		}
-			std::cin.ignore();
 	}
 
-	// Logic
-	for (size_t i = 0; i < IceBox.size(); ++i)
+	// 모든 노드(위치)에 대하여 음료수 채우기
+	int result = 0;
+
+	for (int i = 0; i < N; i++)
 	{
-		Ice* CheckIce;
-
-		// 아직 탐색하지 않은 얼음의 상하좌우를 탐색.
-		if (0 == IceBox[i]->m_Ice && !IceBox[i]->m_Check)
+		for (int j = 0; j < M; j++) 
 		{
-			CheckIce = IceBox[i];
-			CheckIce->m_Check = true;
-
-			// 얼음 모양 구하는 로직
-			while (true)
-			{
-				int block = 0;
-
-				// 네 방향을 체크
-				for (int dir = 0; dir < 4; ++dir)
-				{
-					// 다음 조사할 위치를 정함
-					int nextPos[2] = { CheckIce->pos[X], CheckIce->pos[Y] };
-					switch (dir)
-					{
-					case R:
-						nextPos[X] += 1;
-						break;
-					case L:
-						nextPos[X] -= 1;
-						break;
-					case U:
-						nextPos[Y] -= 1;
-						break;
-					case D:
-						nextPos[Y] += 1;
-						break;
-					}
-
-					// 얼음틀의 위치에서 벗어났을 경우
-					if (nextPos[X] < 0 || nextPos[X] >= N || nextPos[Y] < 0 || nextPos[Y] >= N)
-					{
-						++block;
-						continue;
-					}
-
-					// 존재하는 얼음일 경우
-					else
-					{
-						// 컨테이너 안에서 얼음 찾기
-						for (size_t k = 0; k < IceBox.size(); ++k)
-						{
-							// 얼음을 찾았다면
-							if (IceBox[k]->pos[X] == nextPos[X] && IceBox[k]->pos[Y] == nextPos[Y])
-							{
-								// 막혀야 하는 곳인지 확인
-								if (!IceBox[k]->m_Check && IceBox[k]->m_Ice == 0)
-								{
-									// 해당 얼음을 방문한 곳으로 바꿔주고 다음 탐색할 얼음으로 설정
-									CheckIce = IceBox[k];
-									CheckIce->m_Check = true;
-									dir = -1;
-								}
-
-								// 이미 방문한 얼음이라면 못가는 곳으로 표시
-								else
-								{
-									++block;
-								}
-
-								break;
-							}
-						}
-					}
-				}
-
-				// 사방이 조사할 수 없는 곳이라면 얼음 생성 끝
-				if (block == 4)
-				{
-					++result;
-					break;
-				}
+			// 현재 위치에서 DFS 수행
+			if (CheckIceBox(i, j, N,M, Graph)) {
+				result += 1;
 			}
 		}
-
-		// 얼음이 아니거나 탐색을 이미 했다면.
-		else
-		{
-			continue;
-		}
-
-		// 방문할 곳이 없으면 다시 이전으로 돌아가서 탐색하는 로직이 엄슴 , ,, 
 	}
-
-	std::cout << result;
-
-	std::vector<Ice*>::iterator iter = IceBox.begin();
-	std::vector<Ice*>::iterator iterEnd = IceBox.end();
-
-	// 메모리 해제
-	for (; iter != iterEnd; ++iter)
-	{
-		delete (*iter);
-	}
-
-
-	int a = 0;
+	std::cout << result << '\n'; // 정답 출력 
 }
 
